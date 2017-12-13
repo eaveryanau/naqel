b3452260c9fd741f413e30f33851d016ac7940f1
Artem <artsiom.k@gmail.com>
2017-12-13 11:28:50 +0300

    update helper

--- app/helper.php
+++ app/helper.php
@@ -14,8 +14,11 @@ function waybills_list(){
 }
 
 function custom_processing($order_id){
-    $chosen_methods = WC()->session->get('chosen_shipping_methods');
+    if(WC() && WC()->session){
+        $chosen_methods = WC()->session->get('chosen_shipping_methods');
+    }
  if (get_option('woocommerce_wc_naqel_shipping_method_settings')['enabled'] === 'yes'
+     && $chosen_methods
      && is_array($chosen_methods)
      && in_array('wc_naqel_shipping_method', $chosen_methods)){
         Naqel::createWaybill($order_id);
